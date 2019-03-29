adbb72f4da360ea23fe3dd31f586c201687a144d
Jorge Ignacio Fiestas Lavezzolo <jorge@amigocloud.com>
2019-03-29 15:27:32 -0500

    Improves format and coding standards

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-unordered_map<long long, long long> mem;
+unordered_map<int, int> mem;
 
-long long cycles(long long x){
-	if(mem.find(x) != mem.end()) return mem[x];
-	if(x%2 == 0) mem[x] = 1 + cycles(x/2); 
-	else mem[x] = 2 + cycles((3*x+1)/2);
-	return mem[x];
-}
+int max_cycle(int, int);
+int calculate_cycles(int);
 
 int main(int argc, char *argv[]) {
-	long long i, j;
+	int i, j;
 	mem[1] = 1;
-	while(cin>>i>>j){
-		long long mx= 0;
-		for(long long x = min(i,j); x<= max(i,j); x++)
-			mx =  max(cycles(x), mx);
-		cout<<i<<" "<<j<<" "<<mx<<endl;
-	}
+	while(cin>>i>>j)
+		cout<<i<<" "<<j<<" "<<max_cycle(i,j)<<endl;
     return 0;
+}
+
+int max_cycle(int i, int j){
+	int mx= 0;
+	for(int x = min(i,j); x<= max(i,j); x++)
+		mx =  max(calculate_cycles(x), mx);
+	return mx;
+}
+
+int calculate_cycles(int x){
+	if(mem.find(x) != mem.end()) return mem[x];
+	if(x%2 == 0) mem[x] = 1 + calculate_cycles(x/2); 
+	else mem[x] = 2 + calculate_cycles((3*x+1)/2);
+	return mem[x];
 }
